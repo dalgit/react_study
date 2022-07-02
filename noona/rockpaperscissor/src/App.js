61b74abdfea262ceefa5f5a6adfce4fa168c1d17
junki <junki646@gmail.com>
2022-07-02 22:20:49 +0900

    구현 완료, 주석 추가

--- noona/rockpaperscissor/src/App.js
+++ noona/rockpaperscissor/src/App.js
@@ -17,19 +17,44 @@ const choice = {
   }
 }
 
+const first = {
+  name:"가위바위보 게임",
+  img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIhgKpf_l8q3nuvjne0XicsqT-DXgnCRgEfg&usqp=CAU"
+}
+
 function App() {
-  const [userSelect, setUserSelect] = useState(null)
+  const [userSelect, setUserSelect] = useState(first);
+  const [computerSelect, setComputerSelect] = useState(first);
+  const [result, setResult] = useState("시작해주세요");
+
+  const play= (userChoice)=>{ //ex) 가위를 눌렀을 경우 onclick으로 "scissors"
+    setUserSelect(choice[userChoice]);  //가위바위보중 하나로 셋
+    let computerChoice= randomChoice(); //가위바위보 객체중 하나를 받아옴
+    setComputerSelect(computerChoice); //셋
+    setResult(judgement(choice[userChoice],computerChoice))
+  };
+
+  const judgement = (user,computer)=>{
+    if(user.name === computer.name){
+      return "tie"
+    } else if(user.name==="Rock") return computer.name === "Scissors" ? "win" : "lose"
+    else if(user.name==="Scissors") return computer.name ==="Paper" ? "win" : "lose"
+    else if(user.name==="Paper") return computer.name ==="Rock" ? "win" : "lose"
+  };
 
 
-  const play= (userChoice)=>{
-    setUserSelect(choice[userChoice])
+  const randomChoice=()=>{
+    let itemArray = Object.keys(choice)//choice 객체의 키 값만 뽑은게 itemarray로
+    let randomItem = Math.floor(Math.random()*itemArray.length); //랜덤하게 숫자를 정함
+    let final = itemArray[randomItem]; //키 값(가위,바위,보)가 있는 아이템어레이중 하나를 final에
+    return choice[final]; //ex) choice[rock]
   };
 
   return (
     <div>
       <div className="main">
-        <Box title="You" item={userSelect}/>
-        {/* <Box title="Computer"/> */}
+        <Box title="You" item={userSelect} result={result}/>
+        <Box title="Computer" item={computerSelect} result={result}/>
       </div>
       <div className="main">
         <button onClick={()=>play("scissors")}>가위</button>
--- noona/rockpaperscissor/src/component/Box.js
+++ noona/rockpaperscissor/src/component/Box.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
 const Box = (props) => {
+  let result;
+  if (props.title === "Computer" && props.result !== "tie" && props.result !== "시작해주세요"){
+    result = props.result === "win"?"lose":"win";
+  }else{
+    result = props.result;
+  }
+  
   return (
-    <div className="box">
+    <div className={"box" + result}>
         <h1>{props.title}</h1>
+        <h2 data-testid="item-name">{props.item && props.item.name}</h2>
         <img className="itme-img" src={props.item && props.item.img} alt="profilea"></img>
-        <h2>Win~</h2>
+        <h2>{result}</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Box
+export default Box;
