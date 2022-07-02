a0d386cf93a04ac667b2dd2df45ecd83c225f1b5
junki <junki646@gmail.com>
2022-07-03 07:02:00 +0900

    최종 수정

--- noona/rockpaperscissor/src/component/Box.js
+++ noona/rockpaperscissor/src/component/Box.js
@@ -9,7 +9,9 @@ const Box = (props) => {
   }
   
   return (
-    <div className={"box" + result}>
+    //result에 따라서 box클래스와 win/lose/tie 클래스 생성
+    //백틱(``)을 사용하면 ${}를 사용해 문자열과 변수를 같이 사용 가능
+    <div className={`box ${result}`}>
         <h1>{props.title}</h1>
         <h2 data-testid="item-name">{props.item && props.item.name}</h2>
         <img className="itme-img" src={props.item && props.item.img} alt="profilea"></img>
